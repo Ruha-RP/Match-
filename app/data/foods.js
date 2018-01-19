cd3309aafe25db44192fa631d6dbd9f6519c5523
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-18 21:21:12 -0500

    adds comfortFoodArray to foods.js file

---
+++ app/data/foods.js
@@ -0,0 +1,41 @@
+//===================
+//COMFORT FOODS ARRAY
+//===================
+
+var comfortFoodArray = [
+	{
+		"name":"Pizza",
+		// "photo": "photolink",
+		"scores": [
+			5,
+		    1,
+		    2,
+		    5,
+		    5,
+		    5,
+		    5,
+		    0,
+		    2,
+		    5
+		]
+	},//pizza
+	{
+		"name":"Ice Cream",
+		// "photo": "photolink",
+		"scores": [
+			0,
+		    5,
+		    0,
+		    5,
+		    2,
+		    0,
+		    0,
+		    5,
+		    5,
+		    5
+		]
+	},//ice cream
+];
+
+//exporting the array
+module.exports = comfortFoodArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,15 +7,19 @@ var express = require("express");
 var app = express();
 
 //===========
-//HTML ROUTES
+//DATA SOURCE
+//===========
+
+//===========
+//API ROUTES
 //===========
 
 module.exports = function(app) {
 
 
-	app.get("/api/", function(req, res) {
+	app.get("/api/foods", function(req, res) {
 
-		res.sendFile(path.join(__dirname, "../public/survey.html"));
+		res.sendFile(path.join(__dirname, "../data/foods.js"));
 
 	});
 
