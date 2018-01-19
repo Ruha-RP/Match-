25464e897e1cefd052eb4dbd27cd4f727f005828
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-18 20:39:54 -0500

    diverts to server to actual html pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,17 +15,16 @@ module.exports = function(app) {
 
 	app.get("/", function(req, res) {
 
-		res.send("Hello New User!");
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 
 	});
+
+	app.get("/survey", function(req, res) {
+
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+	});
+
+
 }
 
-
-
-var htmlRoutes = {
-
-}
-
-app.get("/", function(req,res) {
-	res.send("Hello World!")
-});
